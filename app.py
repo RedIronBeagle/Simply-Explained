dc2234910106e1f8a6faf06a6d18ba84d1245de0
RedIronBeagle <ciecor100@gmail.com>
2026-09-09 16:44:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -748,7 +748,7 @@ UI_TEXT = {
 
 LANGUAGES = list(UI_TEXT.keys())
 selected_lang = st.sidebar.selectbox("Select Language", LANGUAGES, index=0)
-    texts = UI_TEXT[selected_lang]
+texts = UI_TEXT[selected_lang]
 
     # Sidebar Help Expander
     st.sidebar.markdown("---")
