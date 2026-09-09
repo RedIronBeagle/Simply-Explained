66a16e9dffd9cbf91b7e7dd519cb735b6667116c
RedIronBeagle <ciecor100@gmail.com>
2026-09-08 19:04:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2011,7 +2011,8 @@ with tab3:
                 end_btn_label = texts.get("end_suffering_btn_title", "🔴 END MY SUFFERING 💀")
 
     if st.button(end_btn_label, key="btn_end_my_suffering_tab3_unique", use_container_width=True):
-        # your click logic
+		
+    # your click logic
 
     st.markdown("<br>", unsafe_allow_html=True)
     st.markdown(
