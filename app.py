de2a37dfe3630368005551634af054a5d5082dd7
RedIronBeagle <ciecor100@gmail.com>
2026-09-09 16:33:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -821,16 +821,9 @@ with st.sidebar:
         st.markdown("---")
         st.markdown(f"**{current_help['step3_title']}**\n{current_help['step3_desc']}")
 		
-	LANGUAGES = list(UI_TEXT.keys())
-	TTS_LANG_MAP = {
-		"English": "en",
-		"Spanish": "es",
-		"German": "de",
-		"French": "fr",
-		"Hindi": "hi",
-		"Mandarin": "zh-CN",
-		"Japanese": "ja",
-		"Korean": "ko",
+     LANGUAGES = list(UI_TEXT.keys())
+     selected_lang = st.sidebar.selectbox("Select Language", LANGUAGES, index=0)
+     texts = UI_TEXT[selected_lang]
 }
 
 
