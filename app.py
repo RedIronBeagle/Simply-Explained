ea018d23dbbb23c377e32d99e7f223b1e1b82304
RedIronBeagle <ciecor100@gmail.com>
2026-09-11 11:48:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1535,7 +1535,7 @@ with tab1:
 # [SECTION 7: MAIN TAB NAVIGATION SETUP & INTERFACES]
 # ==============================================================================
 
-if is_streamlit = True
+if is_streamlit=True
     tab1, tab2, tab3 = st.tabs(
         [texts["tab1_name"], texts["tab2_name"], texts["tab3_name"]]
     )
