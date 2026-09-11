922b46a3a55e74e4ca8901f746fdb61662f74ab7
RedIronBeagle <ciecor100@gmail.com>
2026-09-11 14:47:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1537,7 +1537,7 @@ with tab1:
 if is_streamlit==True:
     tab1, tab2, tab3 = st.tabs(
         [texts["tab1_name"], texts["tab2_name"], texts["tab3_name"]]
-    )
+)
 
 # ==============================================================================
 # [SECTION 8: TAB 1 - MAIN TOPIC SIMPLIFIER INTERFACE]
