5ccf28204b3a62365263d90c7a60fb5ca2eb4993
RedIronBeagle <ciecor100@gmail.com>
2026-09-13 17:32:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1719,8 +1719,7 @@ if submitted:
         vertexai=True,
         project="238164610704",  # Replace with your actual GCP project ID
         location="us-central1",
-
-	    )
+    )
 	
   #  topic = st.text_input("Enter a topic or question:")
     topic = st.text_input("Question:")
