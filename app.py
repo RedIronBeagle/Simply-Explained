ce25f1b0f4bae9e6944323958ecfb1e1aef7a664
RedIronBeagle <ciecor100@gmail.com>
2026-09-13 20:10:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1712,7 +1712,7 @@ with st.form("simply_explained_form"):
 # The submit button creates the 'submitted' boolean when clicked
 # submitted = st.form_submit_button("Generate Explanation")
     if submitted:
-    client = genai.Client(AQ.Ab8RN6KawOhVaJ9IPuREdJ-qd2k95nzMNm4mHTF25UvxtbHgNg),
+        client = genai.Client(AQ.Ab8RN6KawOhVaJ9IPuREdJ-qd2k95nzMNm4mHTF25UvxtbHgNg),
         vertexai=True,
         project="238164610704",  # Replace with your actual GCP project ID
         location="us-central1",
