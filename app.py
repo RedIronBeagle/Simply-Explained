e2994d3d6f1c7e00a7a956b3902be20d98912140
RedIronBeagle <ciecor100@gmail.com>
2026-09-17 14:41:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1935,6 +1935,7 @@ with tab1:
                             clean_text_for_speech = output_text
                             clean_text_for_speech = re.sub(r'[#*`_-]', ' ', clean_text_for_speech)
                             clean_text_for_speech = re.sub(r'\s+', ' ', clean_text_for_speech).strip()
+							
 							tts = gTTS(
 								text=clean_text_for_speech,
 								lang=TTS_LANG_MAP.get(selected_lang, "en"),
