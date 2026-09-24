cae1e8f7a0f3c0b205345b8c4323dea27a761f23
RedIronBeagle <ciecor100@gmail.com>
2026-09-24 17:07:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1788,7 +1788,7 @@ if is_streamlit:
             texts.get("tab1_name", "Simply Explained"),
             texts.get("tab2_name", "Fine-Print Decoder"),
             texts.get("tab3_name", "Tactical Lab"),
-            texts.get("tab4_name", "Global AI Dashboard")
+            texts.get("tab4_name", "⏱️ Global AI Dashboard")
         ]
     )
 
